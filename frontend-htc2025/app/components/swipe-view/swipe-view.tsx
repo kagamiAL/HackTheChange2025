522165635c8b9e80d518785000359a9324776cf9
30206049 <mohammad.hashmi1@ucalgary.ca>
2025-11-08 18:50:49 -0500

    Made it look better

--- frontend-htc2025/app/components/swipe-view/swipe-view.tsx
+++ frontend-htc2025/app/components/swipe-view/swipe-view.tsx
@@ -1,15 +1,18 @@
+import * as React from 'react';
+import cover from '../../../assets/mountains.jpeg';
+
 export default function SwipeView() {
   return (
-    <div className="max-w max-h-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden pb-10">
+    <div className="max-w max-h-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden min-h-[75vh]">
 
   <img
-    src="placeholder.jpg"
+    src={cover.src}
     alt="Event Image"
     className="w-full h-56 object-cover"
   />
 
   <div className="p-6">
-    <span className="bg-gray-100 text-gray-700 text-sm font-medium px-3 py-1 rounded-full">
+    <span className="bg-gray-200 text-gray-900 text-sm font-medium px-3 py-1 rounded-full">
       Environment
     </span>
 
@@ -22,22 +25,22 @@ export default function SwipeView() {
       Join us for a morning beach cleanup to protect marine life and keep our shores beautiful.
     </p>
 
-    <div className="mt-4 flex items-center text-gray-500 text-sm space-x-4">
-      <div className="flex items-center space-x-1">
+    <div className="mt-4 flex flex-col text-gray-500 text-sm space-x-4">
+      <div className="flex items-center space-x-1 mt-2">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-width="2" d="M12 2C8.134 2 5 5.134 5 9c0 7 7 13 7 13s7-6 7-13c0-3.866-3.134-7-7-7z" />
         </svg>
         <span>Santa Monica Beach, CA</span>
       </div>
 
-      <div className="flex items-center space-x-1">
+      <div className="flex items-center space-x-1 mt-2">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2v-7H3v7a2 2 0 002 2z" />
         </svg>
         <span>Nov 12, 2025</span>
       </div>
 
-      <div className="flex items-center space-x-1">
+      <div className="flex items-center space-x-1 mt-2">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
--- frontend-htc2025/app/page.tsx
+++ frontend-htc2025/app/page.tsx
@@ -3,9 +3,8 @@ import * as React from 'react'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-zinc-50">
+    <div className="max-h-screen bg-zinc-50">
       <main className="mx-auto flex max-w-6xl flex-col gap-4 px-6 py-16 text-muted-foreground">
-        <p>Code here!</p>
         <SwipeView />
       </main>
     </div>
