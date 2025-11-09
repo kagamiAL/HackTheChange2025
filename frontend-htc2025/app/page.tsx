e363189db2eb5c7b26e2d9c00da2de14d1677352
30206049 <mohammad.hashmi1@ucalgary.ca>
2025-11-08 21:16:20 -0500

    component is done, removing from page to avoid merge conflict

--- frontend-htc2025/app/page.tsx
+++ frontend-htc2025/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className="max-h-screen bg-zinc-50">
       <main className="mx-auto flex max-w-6xl flex-col gap-4 px-6 py-16 text-muted-foreground">
-        <SwipeView />
       </main>
     </div>
   );
